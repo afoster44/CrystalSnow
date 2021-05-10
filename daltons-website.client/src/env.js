e9760218e215778023d7386371962904403392d7
afoster44 <alexfost94@gmail.com>
2021-05-10 11:24:34 -0600

    Full Stack Crystal Snow/Dalton Ottley auth/server

---
+++ daltons-website.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'dev-osr4rc78.us.auth0.com'
+export const audience = 'https://www.idahosnowremoval.com'
+export const clientId = 'dwIxWz69E7r6k1aW8zKXLaksnr1RClgm'
